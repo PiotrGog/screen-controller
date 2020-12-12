18852478fbb99fb5eb3f22117807664f929782f1
Piotr Gogola <gogola.piotr@interia.pl>
2020-12-12 19:54:25 +0100

    Add thread sleep in main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use opencv;
 mod camera;
 mod screen;
+use std::{thread, time};
 
 fn main() -> opencv::Result<()> {
     let maximum_bright_threshold = 110;
+    let sleep_time = time::Duration::from_millis(200);
+
     let mut camera = camera::brightness::Brightness::new()?;
     let mut screen = screen::brightness::Brightness::new(maximum_bright_threshold);
 
@@ -12,5 +15,7 @@ fn main() -> opencv::Result<()> {
     loop {
         let camera_brightness = camera.get_brightness()?;
         screen.set_screens_bright(camera_brightness as f32);
+
+        thread::sleep(sleep_time);
     }
 }
